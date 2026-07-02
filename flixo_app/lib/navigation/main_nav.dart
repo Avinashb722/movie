474ae57578cc138862a6df003319292badf0d22f
Avinashb722 <avinashbiradar222@gmail.com>
2026-07-02 09:07:44 +0530

    chore: swap open_file_plus with compatible open_file dependency

--- flixo_app/lib/navigation/main_nav.dart
+++ flixo_app/lib/navigation/main_nav.dart
@@ -12,7 +12,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:dio/dio.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:open_file_plus/open_file_plus.dart';
+import 'package:open_file/open_file.dart';
 import 'dart:io';
 import '../screens/home_screen.dart';
 import '../screens/search_screen.dart';
